b4a82b51654bada91f873655565d717ab45245ab
Sam Cappleman-Lynes <sam.cappleman-lynes@softwire.com>
2019-08-01 13:48:55 +0100

    Added a test that pawns can move one square

--- chessington/engine/pieces.py
+++ chessington/engine/pieces.py
@@ -35,16 +35,47 @@ class Pawn(Piece):
     """
 
     def get_available_moves(self, board):
-        return []
+        available_moves = []
+        cur_square=board.find_piece(self)
+        cur_row=cur_square.row
+        cur_col=cur_square.col
+
+        move_direction = 1 if self.player == Player.WHITE else -1
+        sq = Square.at(cur_row + move_direction, cur_col)
+        available_moves.append(sq)
+# 2 spaces on firstmove
+#        sq = Square.at(cur_row + (2*move_direction), cur_col )
+#        available_moves.append(sq)
+
+        return available_moves
 
 
 class Knight(Piece):
     """
     A class representing a chess knight.
     """
-
     def get_available_moves(self, board):
-        return []
+
+        available_moves = []
+        cur_square=board.find_piece(self)
+        cur_row=cur_square.row
+        cur_col=cur_square.col
+
+        knight_row=[-2, -1, 1, 2]
+        knight_col=[1, 2, 2, 1]
+        for x in range(0, len(knight_row) ):
+            poss_row=cur_row+knight_row[x]
+            poss_col=cur_col+knight_col[x]
+            if poss_col>=0 and poss_col<8 and poss_row>=0 and poss_row<8:
+                sq = Square.at(poss_row, poss_col)
+                available_moves.append(sq)
+            poss_row=cur_row-knight_row[x]
+            poss_col=cur_col-knight_col[x]
+            if poss_col>=0 and poss_col<8 and poss_row>=0 and poss_row<8:
+                sq = Square.at(poss_row, poss_col)
+                available_moves.append(sq)
+
+        return available_moves
 
 
 class Bishop(Piece):
@@ -60,9 +91,10 @@ class Rook(Piece):
     """
     A class representing a chess rook.
     """
-
     def get_available_moves(self, board):
-        return []
+
+        available_moves = []
+        return available_moves
 
 
 class Queen(Piece):
